1e5cc4e4ed832a4d689462a2958e78b429d23815
ycao2 <ycao@walmartlabs.com>
2015-04-18 09:04:19 -0700

    updat readme

--- README.md
+++ README.md
@@ -3,4 +3,19 @@
 ### Install
 - npm install 
 - npm start
-- open http://127.0.0.1:8000
+
+### Content
+1 Build a proxy server and setup logging
+2 Develop a command-line interface (CLI) to configure the server
+3 (TODO) Extend your proxy server with additional functions 
+
+### Test
+CLI:
+nodemon --exec babel-node -- index.js --url http://www.google.com
+nodemon --exec babel-node -- index.js
+nodemon --exec babel-node -- index.js --logs=/tmp/proxy-server.log
+ 
+Test:
+curl -v http://127.0.0.1:8001 -H "x-destination-url: http://www.google.com" -d "asdf"
+curl -v http://127.0.0.1:8001 -H "x-asdf: test" -d "asdf"
+cat /tmp/proxy-server.log
--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 /*
  * @ Codepath pre-work: proxy server
- * 1 Build a proxy server and setup logging
- * 2 Develop a command-line interface (CLI) to configure the server
- * 3 Extend your proxy server with additional functions
- *
- * Run:
- * npm start //or nodemon --exec babel-node -- index.js
- *
- *
- * CLI:
- * nodemon --exec babel-node -- index.js
- * nodemon --exec babel-node -- index.js --url http://www.google.com
- * nodemon --exec babel-node -- index.js --logs=/tmp/proxy-server.log
- *
- * Test:
- * curl -v http://127.0.0.1:8001 -H "x-destination-url: http://www.google.com" -d "asdf"
- * curl -v http://127.0.0.1:8001 -H "x-asdf: test" -d "asdf"
- *
- * TODO:
- * - babel ?
- * - req.pipe(res)
- * req.pipe(request(options)).pipe(res)
- * - `http://${destinationUrl}${req.url}`
  */
 let http = require('http')
 let fs = require('fs')
@@ -35,19 +13,17 @@ let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logStream = argv.logs ? fs.createWriteStream(argv.logs) : process.stdout
 
-console.log(argv);
-
 // Simple http server on 8000
 http.createServer((req, res) => {
-  logStream.write('\nEcho request:\n' + JSON.stringify(req.headers))
-  // take request header and set to response header
+  logStream.write('\nEcho request: \n' + JSON.stringify(req.headers))
   for (let header in req.headers) {
     res.setHeader(header, req.headers[header])
   }
   through(req, logStream, {autoDestroy: false})
   req.pipe(res)
 }).listen(8000)
-logStream.write('Listening at http://127.0.0.1:8000')
+
+logStream.write('Listening at http://127.0.0.1:8000\n')
 
 // Proxy http server on 8081
 http.createServer((req, res) => {
@@ -59,8 +35,10 @@ http.createServer((req, res) => {
     headers: req.headers,
     url: url + req.url
   }
-  logStream.write('\nProxy request:\n' + JSON.stringify(req.headers))
+
+  logStream.write('\nProxy request: \n' + JSON.stringify(req.headers))
   through(req, logStream, {autoDestroy: false})
+
   let destinationResponse = req.pipe(request(options))
   logStream.write(JSON.stringify(destinationResponse.headers))
   destinationResponse.pipe(res)
